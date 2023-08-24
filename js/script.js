6713977df800455ff91ece4b7822c2852b31a632
Feisal Migo Keli <kellyfeisal2000@gmail.com>
2023-08-24 22:27:42 +0300

    create hero section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // import images as relative image path won't work with vite/vercel.
-import check from "../assets/check.svg";
-import star from "../assets/star.svg";
-import sushi12 from "../assets/sushi-12.png";
-import sushi11 from "../assets/sushi-11.png";
-import sushi10 from "../assets/sushi-10.png";
+import check from "/sushi_assets/assets/check.svg";
+import star from "/sushi_assets/assets/star.svg";
+import sushi12 from "/sushi_assets/assets/sushi-12.png";
+import sushi11 from "/sushi_assets/assets/sushi-11.png";
+import sushi10 from "/sushi_assets/assets/sushi-10.png";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
